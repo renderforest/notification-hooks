589ad900d62407166909a39e910d274e17ce9cf6
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-12-21 18:36:15 +0400

    test: add sample test to ensure test setup validity.

---
+++ __tests__/test.test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+describe('test: ', () => {
+
+  describe('test(): ', () => {
+    test('check if the test script is working.', () => {
+      expect('test').toBe('test')
+    })
+  })
+
+})
