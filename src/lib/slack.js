5f9c3f7f29c787a2343b140e70edc152ae5123e1
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-12-21 18:15:59 +0400

    lib: add slack lib. provides:
     - notifyError()
     - notifyWarn()
     - notifyInfo()

---
+++ src/lib/slack.js
@@ -0,0 +1,90 @@
+'use strict'
+
+const requestPromise = require('request-promise')
+
+const SLACK_WEB_HOOK_URL = process.env.SLACK_WEB_HOOK_URL
+
+/**
+ * @private
+ * @param {object | string} text
+ * @param {string} channel
+ * @param {string} username
+ * @param {string} icon_emoji
+ * @returns {{channel, text, username, icon_emoji}}
+ * @description Constructs payload for slack.
+ *  Use ``` to send msg as code block.
+ */
+function _payloadForSlack (text, channel, username, icon_emoji) {
+  const msg = typeof text === 'string'
+    ? ['```', text, '```']
+    : ['```', ...Object.keys(text).map(key => `${key}: ${text[key]}`), '```']
+
+  return {
+    channel,
+    text: msg.join('\n'),
+    username,
+    icon_emoji
+  }
+}
+
+/**
+ * @private
+ * @param {object | string} text
+ * @param {string} channel
+ * @param {string} username
+ * @param {string} icon_emoji
+ * @returns {Promise.<>}
+ * @description Send slack msg.
+ */
+function _sendSlackMsg (text, channel, username, icon_emoji) {
+  return requestPromise({
+    url: SLACK_WEB_HOOK_URL,
+    method: 'PUT',
+    form: JSON.stringify(_payloadForSlack(text, channel, username, icon_emoji))
+  })
+}
+
+/**
+ * @public
+ * @param {string} text
+ * @param {string} channel
+ * @param {string} username
+ * @param {string} [icon_emoji=:information_source:]
+ * @return {Promise.<>}
+ * @description Notify error.
+ */
+function notifyError (text, channel, username, icon_emoji = ':fire:') {
+  return _sendSlackMsg(text, channel, username, icon_emoji)
+}
+
+/**
+ * @public
+ * @param {string} text
+ * @param {string} channel
+ * @param {string} username
+ * @param {string} [icon_emoji=:information_source:]
+ * @return {Promise.<>}
+ * @description Notify warn.
+ */
+function notifyWarn (text, channel, username, icon_emoji = ':warning:') {
+  return _sendSlackMsg(text, channel, username, icon_emoji)
+}
+
+/**
+ * @public
+ * @param {string} text
+ * @param {string} channel
+ * @param {string} username
+ * @param {string} [icon_emoji=:information_source:]
+ * @return {Promise.<>}
+ * @description Notify info.
+ */
+function notifyInfo (text, channel, username, icon_emoji = ':information_source:') {
+  return _sendSlackMsg(text, channel, username, icon_emoji)
+}
+
+module.exports = {
+  notifyError,
+  notifyWarn,
+  notifyInfo
+}
