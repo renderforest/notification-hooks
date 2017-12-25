664e470e3396f66db160640ca1d5eac6c9797207
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-12-25 15:54:54 +0400

    build: update dist dir files.

--- dist/lib/slack.js
+++ dist/lib/slack.js
@@ -91,5 +91,9 @@ function notifyInfo (text, channel, username, icon_emoji = ':information_source:
 module.exports = {
   notifyError,
   notifyWarn,
-  notifyInfo
+  notifyInfo,
+  __tests__: {
+    _payloadForSlack,
+    _sendSlackMsg
+  }
 }
