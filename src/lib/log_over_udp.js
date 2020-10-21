968a7d15ecc5b1ba1fbc9c620bc479b7ab4d50f8
gevorg90 <gevonlineplus@gmail.com>
2020-10-21 10:41:13 +0400

    send tag first

--- src/lib/log_over_udp.js
+++ src/lib/log_over_udp.js
@@ -3,7 +3,7 @@ const dgram = require('dgram')
 
 function log_over_UDP (host, port, message, url, tag) {
   const client = dgram.createSocket('udp4')
-  const logObject = url + ' ' + message + ' ' + tag
+  const logObject = tag + ': ' + url + ' ' + message
   const m = Buffer.from(JSON.stringify(logObject))
   client.send(m, 0, m.length, port, host, function (err, bytes) {
     if (err) {
